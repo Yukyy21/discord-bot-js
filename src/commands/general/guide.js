5680e59baa289fd8d76c7e8ef6a32ae6aafebc4b
Nekomaru <elainaleywin@gmail.com>
2026-09-09 09:55:11 +0800

    Make guide command reply ephemeral
    Updated reply to be ephemeral, visible only to the user.

--- src/commands/general/guide.js
+++ src/commands/general/guide.js
@@ -1,9 +1,10 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 const { buildGuide } = require('../../ui/guidePages');
 
 module.exports = {
   data: new SlashCommandBuilder().setName('guide').setDescription('Lihat panduan lengkap cara pakai bot'),
   async execute(interaction) {
-    await interaction.reply(buildGuide('home'));
+    // Ephemeral: cuma pemanggil yang bisa lihat, biar tidak spam channel.
+    await interaction.reply({ ...buildGuide('home'), flags: MessageFlags.Ephemeral });
   },
 };
